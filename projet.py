d32e562c9288d32f8c21ab76db6ed0d0c756c17d
Blackbird0_o <arduino.delloye@gmail.com>
2019-11-08 18:09:19 +0100

    debut du projet

---
+++ projet.py
@@ -0,0 +1,123 @@
+#-------------Imports-------------
+from keras import backend as K
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import keras
+from keras.models import Sequential
+from keras.layers import Activation, Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import BatchNormalization
+from keras.optimizers import RMSprop, adam
+from keras.utils import to_categorical
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from mpl_toolkits.mplot3d import Axes3D 
+from matplotlib import cm
+
+
+#plt.close('all')
+
+
+#-------------Preliminray Data Exploration-------------
+# Loading datas
+data_train = pd.read_csv('exoTrain.csv')
+data_test = pd.read_csv('exoTest.csv')
+
+# transformation des label en array de 0 et 1
+y_train = np.array(data_train["LABEL"])-1
+y_test = np.array(data_test['LABEL'])-1
+
+# on charge les features
+x_train = np.array(data_train.drop('LABEL',axis=1))
+x_test = np.array(data_test.drop('LABEL',axis=1))
+
+# création du vecteur temps (h)
+t = np.arange(len(x_train[0])) * (36.0/60.0)
+dt = 36* 60 # sampling rate (s) les données sont prises avec 36min d'écart
+
+f = np.fft.fftfreq(x_train.shape[1],dt) # vecteur fréquence en (Hz)
+
+#Bootstrapping
+x_train1 = x_train[np.where(y_train == 1)[0]] #Separation du train_set selon le label
+x_train0 = x_train[np.where(y_train == 0)[0]]
+index_train = np.random.randint(0,x_train1.shape[0] , size=x_train0.shape[0]) #genere une liste d'index 
+                                                                          #aléatoire pour equilibrer les données
+x_train_1_boot = x_train1[index_train]
+y_train_boot = np.concatenate((np.ones(x_train0.shape[0]),np.zeros(x_train0.shape[0]))) #on génère une liste de labels avec autant de 1 que de 0
+x_train_boot = np.concatenate((x_train_1_boot,x_train0)) #on rassemble les données une fois équilibrées
+
+#shuffle
+index = np.arange(y_train_boot.shape[0])
+np.random.shuffle(index)
+x_train_boot = x_train_boot[index]
+y_train_boot = y_train_boot[index]
+
+# On passe dans l'espace de fourier
+x_train_fft = np.abs(np.fft.fft(x_train_boot))
+x_test_fft = np.abs(np.fft.fft(x_test))
+# selection des 10 1ere harmoniques
+train_feature = x_train_fft#[0:,0:10]
+test_feature = x_test_fft#[0:,0:10]
+
+
+# Scaling
+x_train_sc = StandardScaler().fit_transform(x_train_boot)
+x_train_fft_sc = StandardScaler().fit_transform(x_train_fft)
+
+#-------------PCA------------- 
+pca = PCA(n_components=10)
+x_PCA = pca.fit_transform(x_train_sc)
+
+# lets visualize datas in 3d
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlabel('Principal Component 1', fontsize = 15)
+ax.set_ylabel('Principal Component 2', fontsize = 15)
+ax.set_zlabel('Principal Component 3', fontsize = 15)
+ax.set_title('ACP du signal temporel', fontsize = 20)
+targets = [0,1]
+colors = ['b', 'r']
+plot_samples = 500
+x_PCA_plot = x_PCA[0:plot_samples]
+
+for target, color in zip(targets,colors):
+    indexes = np.where(y_train_boot[0:plot_samples] == target)
+    ax.scatter(x_PCA_plot[indexes,0]
+               , x_PCA_plot[indexes,1],
+               x_PCA_plot[indexes,2]
+               , c = color
+               , s = 50)
+ax.legend(['pas d\'exoplanetes', 'exoplanetes'])
+ax.grid()
+
+
+
+
+#-------------PCA + FFT------------- 
+pca = PCA(n_components=10)
+x_PCA_fft = pca.fit_transform(x_train_fft_sc)
+
+# lets visualize datas in 3d
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlabel('Principal Component 1', fontsize = 15)
+ax.set_ylabel('Principal Component 2', fontsize = 15)
+ax.set_zlabel('Principal Component 3', fontsize = 15)
+ax.set_title('ACP du signal fréquentiel', fontsize = 20)
+targets = [0,1]
+colors = ['b', 'r']
+plot_samples = 500
+x_PCA_fft_plot = x_PCA_fft[0:plot_samples]
+
+for target, color in zip(targets,colors):
+    indexes = np.where(y_train_boot[0:plot_samples] == target)
+    ax.scatter(x_PCA_fft_plot[indexes,0]
+               , x_PCA_fft_plot[indexes,1],
+               x_PCA_fft_plot[indexes,2]
+               , c = color
+               , s = 50)
+ax.legend(['pas d\'exoplanetes', 'exoplanetes'])
+ax.grid()
+
